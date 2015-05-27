aa0e6e745ed760030d151e3f1e8ab63bd602e9b6
Judah Lamm <Judah.Lamm@gmail.com>
2015-05-26 19:40:35 -1000

    solution to part 1 of the Challenge Multiples of 3 and 5

--- multiples.js
+++ multiples.js
@@ -6,7 +6,13 @@
 exports.sumOfAMultiple = function( n ) {
   var sum = 0;
 
-  // do your work here
+  for(var i = n-1; i >= 0; i--){
+    if(i % 3 === 0){
+      sum += i;
+    }else if(i % 5 === 0){
+      sum +=  i;
+    }
+  };
 
   return sum;
 };
